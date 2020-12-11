ac6341cc8759e5a8f61cf902ef17f7b72d2f586b
Victor Di Trani <victorfdt@yahoo.com.br>
2020-12-11 11:21:42 +0100

    Add backend project with initial setup

---
+++ project-manager-backend/src/test/java/nl/victorfdt/projectmanagerbackend/ProjectManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.victorfdt.projectmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
