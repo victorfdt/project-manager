a6d32485ccff3f237de7aef0c3afae3b6aa96846
Victor Di Trani <victorfdt@yahoo.com.br>
2020-12-19 17:19:47 +0100

    Add Route and components

--- project-manager-frontend/src/App.js
+++ project-manager-frontend/src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/layout/Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CreateProjectForm from "./components/project/CreateProjectForm";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/addProject" component={CreateProjectForm} />
+      </div>
+    </Router>
   );
 }
 
---
+++ project-manager-frontend/src/components/project/CreateProjectForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CreateProjectForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Create Project</h1>
+      </div>
+    );
+  }
+}
+
+export default CreateProjectForm;
