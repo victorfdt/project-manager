f4277e871edd49ffbfba837183d2202a012f338b
Victor Di Trani <victorfdt@yahoo.com.br>
2020-12-19 14:40:59 +0100

    Add Bootstrap Font Awesome and some components

--- project-manager-frontend/src/App.js
+++ project-manager-frontend/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
